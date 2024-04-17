d95fbccc8e5c61d8f13a1dc5459645bf6fbee9b3
vhajsman <vaclav.hajsman@hotmail.com>
2024-04-17 20:12:40 +0200

    Fixed a typo

--- src/kernel/memory/paging.asm
+++ src/kernel/memory/paging.asm
@@ -2,11 +2,11 @@ section .text
 
 global paging_enable
 paging_enable:
-    mov cr3
+    mov cr3, eax
 
     mov ebx, cr4        ; read current cr4
     or ebx, 0x10        ; set PSE
-    mov cr4, rbx        ; update cr4
+    mov cr4, ebx        ; update cr4
 
     mov ebx, cr0        ; read current cr0
     or ebx, 0x80000000  ; set PG
