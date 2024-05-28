ea9aaa0ecc43d5da9f3b7f8f164e4df0850b89dc
vhajsman <vaclav.hajsman@hotmail.com>
2024-05-28 17:02:28 +0200

    Remove all conflicting

--- src/kernel/memory/paging.asm
+++ src/kernel/memory/paging.asm
@@ -1,13 +1,13 @@
-section .text
-
-global paging_enable
-paging_enable:
-    mov cr3, eax
-
-    mov ebx, cr4        ; read current cr4
-    or ebx, 0x10        ; set PSE
-    mov cr4, ebx        ; update cr4
-
-    mov ebx, cr0        ; read current cr0
-    or ebx, 0x80000000  ; set PG
-    mov cr0, ebx        ; update cr0
+;   section .text
+;   
+;   global paging_enable
+;   paging_enable:
+;       mov cr3, eax
+;   
+;       mov ebx, cr4        ; read current cr4
+;       or ebx, 0x10        ; set PSE
+;       mov cr4, ebx        ; update cr4
+;   
+;       mov ebx, cr0        ; read current cr0
+;       or ebx, 0x80000000  ; set PG
+;       mov cr0, ebx        ; update cr0
