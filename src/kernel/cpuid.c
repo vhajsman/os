7710da1a1eef6c1974a0ba7f0b376ec4c75a1f82
vhajsman <vaclav.hajsman@hotmail.com>
2024-07-15 15:43:57 +0200

    bugfix

--- src/kernel/cpuid.c
+++ src/kernel/cpuid.c
@@ -3,6 +3,8 @@
 #include "console.h"
 #include "debug.h"
 
+char* cpu_brand;
+
 void cpuid(u32 type, u32 *eax, u32 *ebx, u32 *ecx, u32 *edx) {
     asm volatile("cpuid"
                 : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx)
@@ -37,8 +39,18 @@ int cpuid_info(int print) {
 //        puts("\n");
 //    }
 
+    //debug_message("Brand: ", "cpuid", KERNEL_MESSAGE);
+    //debug_append(brand);
+
     debug_message("Brand: ", "cpuid", KERNEL_MESSAGE);
-    debug_append(brand);
+
+    if(strstr((char*) brand, "QEMU") != NULL) {
+        cpu_brand = "QEMU";
+    } else {
+        cpu_brand = "VBOX";
+    }
+
+    debug_append(cpu_brand);
 
 
     for(type = 0; type < 4; type++) {
@@ -47,10 +59,10 @@ int cpuid_info(int print) {
         if(print)
             printf("CPUID: type:0x%x, eax:0x%x, ebx:0x%x, ecx:0x%x, edx:0x%x\n", type, eax, ebx, ecx, edx);
 
-        itoa(eax_str, 16, eax);
-        itoa(ebx_str, 16, ebx);
-        itoa(ecx_str, 16, ecx);
-        itoa(edx_str, 16, edx);
+        itoa((char*) eax_str, 16, eax);
+        itoa((char*) ebx_str, 16, ebx);
+        itoa((char*) ecx_str, 16, ecx);
+        itoa((char*) edx_str, 16, edx);
 
         itoa(type_str, 16, type);
 
@@ -58,13 +70,13 @@ int cpuid_info(int print) {
         debug_append(type_str);
 
         debug_append(", eax=");
-        debug_append(eax_str);
+        debug_append((char*) eax_str);
         debug_append(", ebx=");
-        debug_append(ebx_str);
+        debug_append((char*) ebx_str);
         debug_append(", ecx=");
-        debug_append(ecx_str);
+        debug_append((char*) ecx_str);
         debug_append(", edx=");
-        debug_append(edx_str);
+        debug_append((char*) edx_str);
     }
 
     // return (strstr(brand, "QEMU") != NULL) ? BRAND_QEMU : BRAND_VBOX;
