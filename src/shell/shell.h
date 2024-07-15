23d6d3496ae2553181b6db078cc989ace26f4e05
vhajsman <vaclav.hajsman@hotmail.com>
2024-07-15 21:41:26 +0200

    deleted:    src/shell/handler.c 	modified:   src/shell/shell.c 	modified:   src/shell/shell.h

--- src/shell/shell.h
+++ src/shell/shell.h
@@ -3,37 +3,37 @@
 
 #include "types.h"
 
-#define MAX_TOKS    30
-
-struct shell_parseout {
-    int tok_count;
-    char* tok_str;
-    char* tok_arr[MAX_TOKS];
-};
-
-struct shell_execcall {
-    char* prompt;
-
-    union {
-        char* executable;
-        char* internalCommand;
-        char* identifier;
-        char* call;
-    };
-
-    size_t argc;    // Argument count
-    char** argv;    // Arguments
-};
-
-int shell_handleUserInput(char* userInput);
-
-void shell();
-void shell_setPrefix(char* prefix);
-
-char* shell_getPrompt();
-
-#define SHELL_CALLPTR(X) \
-    int (*X)(const char* callParams, u8* uref)
-
+// #define MAX_TOKS    30
+// 
+// struct shell_parseout {
+//     int tok_count;
+//     char* tok_str;
+//     char* tok_arr[MAX_TOKS];
+// };
+// 
+// struct shell_execcall {
+//     char* prompt;
+// 
+//     union {
+//         char* executable;
+//         char* internalCommand;
+//         char* identifier;
+//         char* call;
+//     };
+// 
+//     size_t argc;    // Argument count
+//     char** argv;    // Arguments
+// };
+// 
+// int shell_handleUserInput(char* userInput);
+// 
+// void shell();
+// void shell_setPrefix(char* prefix);
+// 
+// char* shell_getPrompt();
+// 
+// #define SHELL_CALLPTR(X) \
+//     int (*X)(const char* callParams, u8* uref)
+// 
 
 #endif
