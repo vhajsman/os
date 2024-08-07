ab0d1c396c79d8c3445714342773d0dd69d86db5
vhajsman <vaclav.hajsman@hotmail.com>
2024-08-07 22:43:07 +0200

    PIT driver remake

--- src/drivers/time/timer.c
+++ src/drivers/time/timer.c
@@ -1,101 +1,146 @@
+// #include "timer.h"
+// #include "ioport.h"
+// #include "kernel.h"
+// #include "console.h"
+// #include "irq/isr.h"
+// #include "irq/irqdef.h"
+// 
+// #include "debug.h"
+// 
+// u32 _ticks = 0;
+// 
+// void pit_irq(REGISTERS* r) {
+//     IGNORE_UNUSED(r);
+// 
+//     _ticks++;
+// }
+// 
+// u8 pit_addressing(u8 counter) {
+//     switch (counter) {
+//         case PIT_COUNTER_0: 
+//             return PIT_REG_COUNTER0;
+//         case PIT_COUNTER_1: 
+//             return PIT_REG_COUNTER1;
+//         case PIT_COUNTER_2: 
+//             return PIT_REG_COUNTER2;
+//         default: 
+//             return PIT_REG_COUNTER0;
+//     }
+// }
+// 
+// void pit_sendCommand(u8 command) {
+//     outportb(PIT_REG_COMMAND, command);
+// }
+// 
+// void pit_sendData (u16 data, u8 counter) {
+//     u8 port = pit_addressing(counter);
+//     
+//     outportb(port, data & 0xFF);         // Low byte
+//     outportb(port, (data >> 8) & 0xFF);  // High byte
+// }
+// 
+// u8 pit_readData (u8 counter) {
+//     return inportb(pit_addressing(counter));
+// }
+// 
+// void pit_init() {
+//     isr_registerInterruptHandler(IRQ_BASE + IRQ0_TIMER, pit_irq);
+//     debug_message("Init OK", "PIT", KERNEL_OK);
+// 
+//     pit_start(1000, PIT_COUNTER_2, PIT_MODE_SQUAREWAVEGEN);
+// }
+// 
+// void pit_start(u32 frequency, u8 counter, u8 mode) {
+//     if (!frequency) return;
+// 
+//     u16 divisor = PIT_CALCULATE_DIVISOR(frequency);
+//     u8 ocw = 0;
+// 
+//     ocw = (ocw & ~PIT_MASK_MODE)    | mode;
+//     ocw = (ocw & ~PIT_MASK_RL)      | PIT_RL_DATA;
+//     ocw = (ocw & ~PIT_MASK_COUNTER) | (counter << 6);
+// 
+//     pit_sendCommand(ocw);
+//     pit_sendData(divisor, counter);  // Stačí zavolat jednou
+// 
+//     _ticks = 0;
+// }
+// 
+// void pit_wait(u32 ticks) {
+//     u32 finish = _ticks + ticks;
+// 
+//     while(_ticks < finish) {}
+// }
+// 
+// u32 pit_directRead() {
+//     u32 count = 0;
+// 
+// 	outportb(0x43, 0);
+//  
+// 	count = inportb(0x40);
+// 	count |= inportb(0x40) << 8;
+//  
+// 	return count;
+// }
+// 
+// void pit_directWrite(u32 val) {
+// 	outportb(0x40,  val & 0xFF);
+// 	outportb(0x40, (val >> 8) & 0xFF);
+//     
+//     _ticks = val;
+// }
+// 
+// void pit_sleep(u32 ms) {
+//     u32 start_ticks = _ticks;
+//     u32 ticks_to_wait = ms;
+// 
+//     debug_message("Sleeping...", "PIT", KERNEL_OK);
+//     while ((_ticks - start_ticks) < ticks_to_wait) {
+//         // Busy-wait loop
+//     }
+//     debug_message("Woke up!", "PIT", KERNEL_OK);
+// }
+// 
+
 #include "timer.h"
 #include "ioport.h"
 #include "kernel.h"
-#include "console.h"
 #include "irq/isr.h"
 #include "irq/irqdef.h"
 
-#include "debug.h"
+void pit_handler(REGISTERS* r);
 
-u32 _ticks = 0;
+// PIT tick counter
+volatile u32 tick = 0;
 
-void pit_irq(REGISTERS* r) {
-    IGNORE_UNUSED(r);
+void pit_init(u32 freq) {
+    u32 divisor = PIT_FREQUENCY / freq;
 
-    _ticks++;
+    // Command byte: channel 0, access mode lobyte/hibyte, operating mode 3 (square wave generator)
+    outportb(PIT_REG_MODE, 0x36);
+
+    // Send the frequency divisor to channel 0 data port
+    outportb(PIT_REG_DATA0, (u8)  (divisor & 0xFF));        // Low byte
+    outportb(PIT_REG_DATA0, (u8) ((divisor >> 8) & 0xFF));  // High byte
+
+    isr_registerInterruptHandler(IRQ_BASE + IRQ0_TIMER, pit_handler);
 }
 
-u8 pit_addressing(u8 counter) {
-    switch (counter) {
-        case PIT_COUNTER_0: 
-            return PIT_REG_COUNTER0;
-        case PIT_COUNTER_1: 
-            return PIT_REG_COUNTER1;
-        case PIT_COUNTER_2: 
-            return PIT_REG_COUNTER2;
-        default: 
-            return PIT_REG_COUNTER0;
+void pit_handler(REGISTERS* r) {
+    IGNORE_UNUSED(r);
+    tick++;
+}
+
+void sleep(u32 ms) {
+    u32 target = tick + ms * (PIT_FREQUENCY / 1000);
+    while (tick < target) {
+        
     }
 }
 
-void pit_sendCommand(u8 command) {
-    outportb(PIT_REG_COMMAND, command);
-}
-
-void pit_sendData (u16 data, u8 counter) {
-    u8 port = pit_addressing(counter);
-
-    outportb(port, data & 0xFF);
-    outportb(port, (data >> 8) & 0xFF);
-}
-
-u8 pit_readData (u8 counter) {
-    return inportb(pit_addressing(counter));
-}
-
-void pit_init() {
-    isr_registerInterruptHandler(IRQ_BASE + IRQ0_TIMER, pit_irq);
-    debug_message("Init OK", "PIT", KERNEL_OK);
-}
-
-void pit_start(u32 frequency, u8 counter, u8 mode) {
-    if(!frequency) 
-        // TODO: Make the timer stop.
-        return;
-    
-    u16 divisor = PIT_CALCULATE_DIVISOR(frequency);
-    u8 ocw = 0;
-
-    ocw = (ocw & ~PIT_MASK_MODE)    | mode;
-	ocw = (ocw & ~PIT_MASK_RL)      | PIT_RL_DATA;
-	ocw = (ocw & ~PIT_MASK_COUNTER) | (counter << 6);
-
-    pit_sendCommand(ocw);
-
-    // ? Set the frequency
-    pit_sendData( divisor       & 0xFF, counter);
-    pit_sendData((divisor >> 8) & 0xFF, counter);
-
-    _ticks = 0;
-}
-
-void pit_wait(u32 ticks) {
-    u32 finish = _ticks + ticks;
-
-    while(_ticks < finish) {}
-}
-
-u32 pit_directRead() {
-    u32 count = 0;
-
-	outportb(0x43, 0);
- 
-	count = inportb(0x40);
-	count |= inportb(0x40) << 8;
- 
-	return count;
-}
-
-void pit_directWrite(u32 val) {
-	outportb(0x40,  val & 0xFF);
-	outportb(0x40, (val >> 8) & 0xFF);
-    
-    _ticks = val;
-}
-
-void pit_sleep(u32 ms) {
-    u32 ticks_to_wait = (PIT_DEFAULT_FREQUENCY * ms) / 1000;
-    u32 start_ticks = _ticks;
-
-    while ((_ticks - start_ticks) < ticks_to_wait) {}
+void wait(u32 ticks) {
+    u32 target = tick + ticks;
+    while (tick < target) {
+        
+    }
 }
--- src/drivers/time/timer.h
+++ src/drivers/time/timer.h
@@ -2,44 +2,71 @@
 #define __TIMER_H
 
 #include "types.h"
-
 #include "devices/pic.h"
 
-#define	PIT_MASK_BINCOUNT           1       //00000001
-#define	PIT_MASK_MODE               0xE     //00001110
-#define	PIT_MASK_RL                 0x30    //00110000
-#define	PIT_MASK_COUNTER            0xC0    //11000000
+// #include "devices/pic.h"
+// #define PIT_DEFAULT_FREQUENCY 18.2065
+// #define	PIT_MASK_BINCOUNT           1       //00000001
+// #define	PIT_MASK_MODE               0xE     //00001110
+// #define	PIT_MASK_RL                 0x30    //00110000
+// #define	PIT_MASK_COUNTER            0xC0    //11000000
+// 
+// #define	PIT_BINCOUNT_BINARY         0       //0         // Use when setting I86_PIT_OCW_MASK_BINCOUNT
+// #define	PIT_BINCOUNT_BCD            1       //1
+// #define	PIT_MODE_TERMINALCOUNT      0       //0000      // Use when setting I86_PIT_OCW_MASK_MODE
+// #define	PIT_MODE_ONESHOT            0x2     //0010
+// #define	PIT_MODE_RATEGEN	        0x4     //0100
+// #define	PIT_MODE_SQUAREWAVEGEN      0x6     //0110
+// #define	PIT_MODE_SOFTWARETRIG       0x8     //1000
+// #define	PIT_MODE_HARDWARETRIG       0xA     //1010
+// #define	PIT_RL_LATCH                0       //000000    // Use when setting I86_PIT_OCW_MASK_RL
+// #define	PIT_RL_LSBONLY              0x10    //010000
+// #define	PIT_RL_MSBONLY              0x20    //100000
+// #define	PIT_RL_DATA                 0x30    //110000
+// #define	PIT_COUNTER_0               0       //00000000  // Use when setting I86_PIT_OCW_MASK_COUNTER
+// #define	PIT_COUNTER_1               0x40    //01000000
+// #define	PIT_COUNTER_2               0x80    //10000000
+// 
+// #define	PIT_REG_COUNTER0            0x40
+// #define	PIT_REG_COUNTER1            0x41
+// #define	PIT_REG_COUNTER2            0x42
+// #define	PIT_REG_COMMAND             0x43
+// 
+// #define PIT_CALCULATE_DIVISOR(frequency) ((1193180 + (frequency) / 2) / (frequency))
+// 
+// #define PIT_BASE_FREQUENCY 1193182  // Base frequency of the PIT in Hz
+// #define TARGET_FREQUENCY 1000
+// 
+// u8 pit_addressing(u8 counter);
+// void pit_init();
+// void pit_start(u32 frequency, u8 counter, u8 mode);
+// 
+// void pit_wait(u32 ticks);
+// void pit_sleep(u32 ms);
+// 
+// u32 pit_directRead();
+// void pit_directWrite(u32 val);
+// 
 
-#define	PIT_BINCOUNT_BINARY         0       //0         // Use when setting I86_PIT_OCW_MASK_BINCOUNT
-#define	PIT_BINCOUNT_BCD            1       //1
-#define	PIT_MODE_TERMINALCOUNT      0       //0000      // Use when setting I86_PIT_OCW_MASK_MODE
-#define	PIT_MODE_ONESHOT            0x2     //0010
-#define	PIT_MODE_RATEGEN	        0x4     //0100
-#define	PIT_MODE_SQUAREWAVEGEN      0x6     //0110
-#define	PIT_MODE_SOFTWARETRIG       0x8     //1000
-#define	PIT_MODE_HARDWARETRIG       0xA     //1010
-#define	PIT_RL_LATCH                0       //000000    // Use when setting I86_PIT_OCW_MASK_RL
-#define	PIT_RL_LSBONLY              0x10    //010000
-#define	PIT_RL_MSBONLY              0x20    //100000
-#define	PIT_RL_DATA                 0x30    //110000
-#define	PIT_COUNTER_0               0       //00000000  // Use when setting I86_PIT_OCW_MASK_COUNTER
-#define	PIT_COUNTER_1               0x40    //01000000
-#define	PIT_COUNTER_2               0x80    //10000000
+#define PIT_FREQUENCY 1193180
 
-#define	PIT_REG_COUNTER0            0x40
-#define	PIT_REG_COUNTER1            0x41
-#define	PIT_REG_COUNTER2            0x42
-#define	PIT_REG_COMMAND             0x43
+// ? I/O port  Usage
+// ? 0x40      Channel 0 data port (read/write)
+// ? 0x41      Channel 1 data port (read/write)
+// ? 0x42      Channel 2 data port (read/write)
+// ? 0x43      Mode/Command register (write only, a read is ignored)
 
-#define PIT_CALCULATE_DIVISOR(frequency) ((1193180 + (frequency) / 2) / (frequency))
+#define PIT_REG_DATA0   0x40
+#define PIT_REG_DATA1   0x41
+#define PIT_REG_DATA2   0x42
+#define PIT_REG_MODE    0x43
+#define PIT_REG_COMMAND 0x43
 
-u8 pit_addressing(u8 counter);
-void pit_init();
-void pit_start(u32 frequency, u8 counter, u8 mode);
+void pit_init(u32 freq);
+void sleep(u32 ms);
+void wait(u32 ticks);
 
-void pit_wait(u32 ticks);
-
-u32 pit_directRead();
-void pit_directWrite(u32 val);
+#define pit_sleep sleep
+#define pit_wait wait
 
 #endif
