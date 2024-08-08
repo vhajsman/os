805294c1b2eb293c30c8f66513e90326c660b736
vhajsman <vaclav.hajsman@hotmail.com>
2024-08-08 22:27:25 +0200

    bugfix

--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,17 @@
-qemu-system-x86_64 -kernel build/bin/krnl.bin -m 64 -chardev stdio,id=char0,logfile=serial.log,signal=off -serial chardev:char0
+#! /bin/sh
+# qemu-system-x86_64 -kernel build/bin/krnl.bin -m 64 -chardev stdio,id=char0,logfile=serial.log,signal=off -serial chardev:char0
+qemu-system-x86_64                                          \
+    -M q35                                                  \
+    -cpu pentium                                            \
+    -smp sockets=1,cores=1,threads=1                        \
+    -m 256                                                  \
+    -vga std                                                \
+    -device ac97                                            \
+    -device rtl8139,netdev=n0                               \
+    -netdev user,id=n0                                      \
+    -accel tcg,thread=multi,tb-size=2048                    \
+    -boot d                                                 \
+    -device intel-iommu                                     \
+    -cdrom build/*.iso                                      \
+    -chardev stdio,id=char0,logfile=serial.log,signal=off   \
+    -serial chardev:char0
