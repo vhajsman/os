4a08e9750e10649c3b455c5bca9c5be85a7032e2
vhajsman <vaclav.hajsman@hotmail.com>
2024-08-09 00:55:25 +0200

    modified:   config/grub.cfg 	modified:   script/imggen.sh

--- script/imggen.sh
+++ script/imggen.sh
@@ -2,6 +2,7 @@ echo "Generating image"
 
 mkdir -p build/iso/boot/grub
 
+cp res/cbos-grub-bg.png build/iso/boot/grub/background.png
 cp config/grub.cfg build/iso/boot/grub/
 cp build/bin/* build/iso/
 
