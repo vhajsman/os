7382fcfa142be21914b86dc417ac0a9c3b56184f
vhajsman <vaclav.hajsman@hotmail.com>
2024-11-02 23:01:13 +0100

    bugfix

--- src/kernel/core/gdt/gdt.asm
+++ src/kernel/core/gdt/gdt.asm
@@ -20,3 +20,31 @@ gdt_load:
     jmp 0x08:far_jump
 far_jump:
     ret
+
+section .data
+align 8
+gdt_start:
+    ; Null segment (0x00)
+    dq 0x0000000000000000
+
+    ; Code segment descriptor (0x08)
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xCF
+    db 0x00
+
+    ; Data segment descriptor (0x10)
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xCF
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
