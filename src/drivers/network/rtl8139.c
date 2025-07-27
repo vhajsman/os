d8fffcd947669dcc4ab5ace8591cd5e0c17d1d47
vhajsman <1816.cookie1816@gmail.com>
2025-07-27 20:14:38 +0200

    fix forgotten header

--- src/drivers/network/rtl8139.c
+++ src/drivers/network/rtl8139.c
@@ -6,19 +6,6 @@
 #include "memory/memory.h"
 #include "ioport.h"
 
-#define RTL8139_VENDOR_ID 0x10EC
-#define RTL8139_DEVICE_ID 0x8139
-
-#define RTL8139_REG_CMD     0x37
-#define RTL8139_REG_RBSTART 0x30
-#define RTL8139_REG_RCR     0x44
-
-#define RTL8139_CMD_RESET   0x10
-#define RTL8139_RX_EN       0x08
-#define RTL8139_TX_EN       0x04
-
-#define RTL8139_RXBUFFER_SIZE ((1024 * 8) + 16 + 1500)
-
 pci_dev_t rtl8139_device;
 u16 rtl8139_io_base;
 u32 rtl8139_io_bar0;
--- src/drivers/network/rtl8139.h
+++ src/drivers/network/rtl8139.h
@@ -1,7 +1,20 @@
 #ifndef __RTL8139_H
 #define __RTL8139_H
 
+#define RTL8139_VENDOR_ID 0x10EC
+#define RTL8139_DEVICE_ID 0x8139
 
+#define RTL8139_REG_CMD     0x37
+#define RTL8139_REG_RBSTART 0x30
+#define RTL8139_REG_RCR     0x44
+
+#define RTL8139_CMD_RESET   0x10
+#define RTL8139_RX_EN       0x08
+#define RTL8139_TX_EN       0x04
+
+#define RTL8139_RXBUFFER_SIZE ((1024 * 8) + 16 + 1500)
+
+void rtl8139_init();
 
 #endif
 
