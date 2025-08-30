395166de7c60c70268896cef563eb5a95dedc444
vhajsman <1816.cookie1816@gmail.com>
2025-08-30 22:50:45 +0200

    unused params bugfix

--- src/kernel/core/debug/stacktrace.c
+++ src/kernel/core/debug/stacktrace.c
@@ -68,7 +68,7 @@ void debug_dumpStackTrace(u8 depth, void (*_fn_print)(unsigned char*)) {
         _fn_print((unsigned char*) i_string);
         _fn_print((unsigned char*) "] 0x");
         _fn_print((unsigned char*) addr_string);
-        _fn_print(": ");
+        _fn_print((unsigned char*) ": ");
         _fn_print((unsigned char*) debug_lookup((uintptr_t) addr));
         _fn_print((unsigned char*) "\n");
     }
